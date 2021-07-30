411baa78b683c2c30454e9b5752b51b822b8513b
nvskumar1 <87903868+nvskumar1@users.noreply.github.com>
2021-07-30 21:47:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
     
+    var rate = document.getElementById("rate").value;
+    
+    var years = document.getElementById("years").value;
+    
+ result = document.getElementById("result");
+  result.innerHTML =( principal * years * rate /100);
+   
 }
         
